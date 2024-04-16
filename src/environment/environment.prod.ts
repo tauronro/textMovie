627d173e1e5fcde1f7fe595a0cabda8db46876a2
gustavo morales <brmorales@alianza.com.co>
2024-04-16 13:41:10 -0500

    Se agrega la configuración de las API

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.themoviedb.org/3',
+  apiKey:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzJkZjcyYTNlMzUwMWE0ZWQ0MWQxODU1NDdjODJiNSIsInN1YiI6IjVmMTk0ZTMwNDQ3ZjljMDAzNDM2NmZiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.e2nwrn17wp4h_3JOtEpgnvWfc1_E-nuRlErOfojmLnw',
+};
